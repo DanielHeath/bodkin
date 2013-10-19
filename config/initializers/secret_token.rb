7241070167b9fba5cfa791483f9442cde952b42d
Daniel Heath <daniel.r.heath@gmail.com>
2013-10-19 11:07:44 +1100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bodkin::Application.config.secret_key_base = 'addce7e2e15786bff9687c03dac4e199d3ad9a8c972d8b8a66e136a9a45fdaa68339e992c5b77dd6e384064f407f42ad22046f9f463b1fbcb46f574df912db03'
